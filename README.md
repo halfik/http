b7fb495e148ea135adb3fcfcff05b6765a32c51a
halfik <halfik@gmail.com>
2016-03-25 12:38:05 +0100

    dev

---
+++ README.md
@@ -0,0 +1,9 @@
+Netinteractive\Http
+===================
+
+Package delivers tools to work with Http requests and responses.
+
+## Services
+ * \Netinteractive\Http\ResponseServiceProvider - service that provides Response object.
+
+## Changelog
---
+++ src/Netinteractive/Http/ResponseServiceProvider.php
@@ -0,0 +1,27 @@
+<?php namespace Netinteractive\Http;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+
+class ResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
